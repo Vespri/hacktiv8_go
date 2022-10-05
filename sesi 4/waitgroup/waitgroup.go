b55ad11bf5fec50c321996b3f7ee89ad76ac252d
Vespri <kresna.vespri21@gmail.com>
2022-10-05 11:53:07 +0700

    add waitgroup go

---
+++ sesi 4/waitgroup/waitgroup.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	// Implementation
+	fruits := []string{"apple", "manggo", "durian", "rambutan"}
+
+	var wg sync.WaitGroup
+
+	for index, fruit := range fruits {
+		wg.Add(1)
+		go printFruit(index, fruit, &wg)
+	}
+
+	wg.Wait()
+}
+
+func printFruit(index int, fruit string, wg *sync.WaitGroup) {
+	fmt.Printf("Index => %d, fruit => %s\n", index, fruit)
+	wg.Done()
+}
